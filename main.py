6c39df0a283306c5aac3290c1593af393ad9fe0e
Kiran9527 <43056819+Kiran952@users.noreply.github.com>
2021-05-12 20:40:13 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pyttsx3
+import PyPDF2  #pacakge for to read the pdf file
+book = open('python cheat sheet.pdf','rb')  #rb = rb : Opens the file as read-only in binary format and starts reading from the beginning of the file.
+pdfReader = PyPDF2.PdfFileReader(book)   #for to read the files
+pages = pdfReader.numPages   #for to print page numbers
+print(pages)
+speaker = pyttsx3.init()  #intilazating the pyttsx3 package
+for num in range (10,pages):
+    page = pdfReader.getPage(9)   #for to read the particular page if u want to read 10 then enter 9 page number
+    text =page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
+
+
+#total page numbers in pdf file is 14
---
+++ text reader.py
@@ -0,0 +1,4 @@
+import pyttsx3
+speaker = pyttsx3.init()  #intilazating the pyttsx3 package
+speaker.say('hey macha I can hear you ')
+speaker.runAndWait()
